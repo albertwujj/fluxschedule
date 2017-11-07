9bd7e54df33b1a119eb3bd9db3c10fe062b639bb
Albert Wu <albertwu@wless-user-172019205120.temp.wsu.edu>
2017-11-07 14:03:16 -0800

    Initial Commit

---
+++ Schedule Maker Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schedule Maker Prototype
+//
+//  Created by Albert Wu on 11/7/17.
+//  Copyright © 2017 Old Friend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
