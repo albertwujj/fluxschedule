5bc9367c9e70575debd039aca07aa4cd2a7e0fff
Albert Wu <albertwujj@gmail.com>
2018-03-18 11:26:57 -0700

    removed second zephyr sync

--- Schedule Maker Prototype/AppDelegate.swift
+++ Schedule Maker Prototype/AppDelegate.swift
@@ -93,7 +93,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if notifPermitted {
             scheduleViewController.scheduleTaskNotifs(withAction: false)
         }
-        Zephyr.sync()
+
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
