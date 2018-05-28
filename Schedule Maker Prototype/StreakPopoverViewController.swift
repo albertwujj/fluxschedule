33c3e36dd14c952be5a5cee357840cec299a68af
Albert Wu <albertwujj@gmail.com>
2018-05-28 02:20:30 -0700

    added new streaks

---
+++ Schedule Maker Prototype/StreakPopoverViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StreakPopoverViewController.swift
+//  Schedule Maker Prototype
+//
+//  Created by Albert Wu on 5/27/18.
+//  Copyright © 2018 Old Friend. All rights reserved.
+//
+
+import Foundation
---
+++ Schedule Maker Prototype/UIDevice.swift
@@ -0,0 +1,9 @@
+//
+//  UIDevice.swift
+//  Schedule Maker Prototype
+//
+//  Created by Albert Wu on 5/16/18.
+//  Copyright © 2018 Old Friend. All rights reserved.
+//
+
+import Foundation
